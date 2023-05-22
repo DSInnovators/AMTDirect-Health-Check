23598d86f9dafad5fe1405a915e4474654162276
FariaRichaDsi <faria.habib@dsinnovators.com>
2023-05-22 18:29:18 +0600

    Minor Changes for Negative Testing

--- cypress/e2e/Pages/dashboardPage.cy.js
+++ cypress/e2e/Pages/dashboardPage.cy.js
@@ -3,7 +3,7 @@ export class DashboardPage {
 
     }
     assertDashboardText() {
-        if (!cy.get('.active').should('be.visible')) {
+        if (!cy.get('.acteve').should('be.visible')) {
             throw new Error('Login Failed');
         }
     }
